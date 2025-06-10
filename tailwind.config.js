ae956708317bca16a20a51d753231deff3261cbc
SocanIcode <azebsame@gmail.com>
2025-06-10 14:01:36 +0200

    Setup and Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./feed/**/*.html",
+    "./profile/**/*.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
